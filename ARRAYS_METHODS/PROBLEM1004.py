ce6119c83ae1803601f3e3bedf2945684f58f2fd
deepu <deepumahapatro2006@gmail.com>
2026-06-02 18:00:33 +0530

    LONGEST CONSECUTIVE ONES

---
+++ ARRAYS_METHODS/PROBLEM1004.py
@@ -0,0 +1,28 @@
+#LONGEST CONSECUTIVE ONES
+from typing import List
+class Solution():
+    def longestOnes(self, nums: List[int], k: int)->int:
+        #EDGE CASE
+        if not nums:
+            return 0
+        #INITIALIZE VARIABLES
+        left=0
+        max_length=0
+        zero_count=0
+        #EXPAND WINDOW
+        for right in range(len(nums)):
+            #COUNT ZEROS
+            if nums[right]==0:
+                zero_count+=1
+            #TOO MANY ZEROS
+            while zero_count>k:
+                if nums[left]==0:
+                    zero_count-=1
+                left+=1
+            #UPDATE MAX_LENGTH
+            max_length=max(max_length,right-left+1)
+        return max_length
+obj=Solution()
+nums=[1,1,0,1,0]
+k=1
+print(obj.longestOnes(nums,k))
